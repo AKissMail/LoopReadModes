84881eeb761b3d2af563e7b306da8bec530cbd71
321501 <KLNotbf22xMrt5VzFSAwdkmSbH+--+>
2022-06-16 12:57:58 +0200

    Coding Style MediaWiki eingefügt

--- includes/LoopReadModes.php
+++ includes/LoopReadModes.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @description This file is the base for the LoopReadModes Extension. It contains three functions to handle the used hooks.
+ * @description This file is the base for the LoopReadModes Extension. It contains three functions to handle the used
+ * hooks.
  * @author Andreas Kißmehl
  * @version 0.1
  */
@@ -17,23 +18,25 @@ class LoopReadModes
     public static function onExtensionLoad(){}
 
     /**
-	 * The used BeforePageDisplay Hook is handled in this function. It adds a JavaScript Modul and a CSS file to the output what is send to the Client.
+	 * The used BeforePageDisplay Hook is handled in this function. It adds a JavaScript Modul and a CSS file to the
+	 * output what is send to the Client.
      * @param $out * OutputPage object.
      * @return void
      */
 
-    public static function onBeforePageDisplay( $out, $skin)
+    public static function onBeforePageDisplay( $out, $skin )
     {
-		$out->addModules( ['extension.LoopReadModes.js', 'extension.LoopReadModes']);
+		$out->addModules( [ 'extension.LoopReadModes.js', 'extension.LoopReadModes' ] );
     }
 
 	/**
-	 * The used BeforePageDisplay Hook is handled in this function. It adds a JavaScript Modul and a CSS file to the output what is send to the Client.
+	 * The used BeforePageDisplay Hook is handled in this function. It adds a JavaScript Modul and a CSS file to the
+	 * output what is send to the Client.
 	 * @param $output * OutputPage object.
 	 * @return void
 	 */
-	public static function onSpecialSearchResultsAppend($specialSearch, $output, $term)
+	public static function onSpecialSearchResultsAppend( $specialSearch, $output, $term )
 	{
-		$output->addModules( ['extension.LoopReadModes.js', 'extension.LoopReadModes']);
+		$output->addModules( [ 'extension.LoopReadModes.js', 'extension.LoopReadModes' ] );
 	}
 }
